087499c236b5ec5a1be227a1a7befb06a34ff518
shofira <shofirakhairunnisa@gmail.com>
2021-07-24 11:51:29 +0700

    update skill and cv

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="skill" class="mt-20 md:mt-32 mb-20 dark:text-light">
     <h3 class="text-2xl text-center">My Skills</h3>
-    <div class="grid grid-cols-3 md:flex items-center justify-around text-center">
+    <div class="grid grid-cols-3 md:grid-cols-5 items-center justify-around text-center">
       <div v-for="(x,y) in skill" :key="y">
         <Icon :icon="x[0]" class="text-6xl mt-8" />
-        <p class="text-xs">{{ x[1] }}</p>
+        <p class="text-xs mt-2">{{ x[1] }}</p>
       </div>
-    </div>
     <div class="text-center mt-8">
       <img src="../static/images/tw.png" class="mx-auto w-15 h-10 my-3">
       <p class="text-xs">Tailwind</p>
+    </div>
     </div>
   </div>
 </template>
@@ -24,6 +24,7 @@ export default {
         ['lab la-vuejs text-green-600', 'Vue Js'],
         ['lab la-node text-green-800', 'Node Js'],
         ['lab la-laravel text-red-600', 'Laravel'],
+        ['lab la-git text-red-600', 'GIT / GITHUB'],
         ['lab la-bootstrap text-purple-800', 'Bootstrap'],
       ],
     }
